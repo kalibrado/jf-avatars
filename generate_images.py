9d6fa932530a5d1b8d80eda898544ab1c3c1596d
kalibrado <leonardo.fonseca85300@gmail.com>
2024-10-16 19:32:08 +0200

    init

--- generate_images.py
+++ generate_images.py
@@ -1,9 +1,17 @@
 import os
 import json
-import argparse
+import sys
 
-# Fonction pour générer les listes d'images et de sections
+images_dir = "src/img"  # Dossier contenant les images
+js_output = "src/js/constants.js"  # Chemin vers le fichier de sortie JS
+package_json_path = "package.json"  # Chemin vers le fichier package.json
+
+def get_base_url(version):
+    """Génère l'URL de base en fonction de la version.""" 
+    return f"https://monsite/npm_dist/{version}/"
+
 def generate_image_and_section_lists(images_dir, base_url):
+    """Génère les listes d'images et de sections à partir des fichiers dans le dossier d'images."""
     srcImages = set()
     options = set()
 
@@ -23,8 +31,8 @@ def generate_image_and_section_lists(images_dir, base_url):
 
     return image_files_list, sections_list
 
-# Fonction pour mettre à jour le fichier JavaScript
 def update_js_file(src_images, options, output):
+    """Met à jour le fichier JavaScript avec les listes d'images et de sections."""
     src_images_str = json.dumps(src_images, ensure_ascii=False, indent=4)
     sections_str = json.dumps(options, ensure_ascii=False, indent=4)
 
@@ -43,17 +51,15 @@ export const constants = {
 
     print(f"Fichier {output} mis à jour avec succès.")
 
-# Main execution
+# Exécution principale
 if __name__ == "__main__":
-    # Configuration de l'analyseur d'arguments
-    parser = argparse.ArgumentParser(description='Générer des listes d\'images et mettre à jour le fichier constants.js.')
-    parser.add_argument('base_url', type=str, help='L\'URL de base pour les images générées.')
-    
-    args = parser.parse_args()
+    # Vérifiez si un argument de version a été passé
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <version>")
+        sys.exit(1)
 
-    images_dir = "src/img"
-    js_output = "src/js/constants.js"
-    base_url = args.base_url
+    version = sys.argv[1]  # Récupère la version depuis l'argument
+    base_url = get_base_url(version)
 
     src_images, options = generate_image_and_section_lists(images_dir, base_url)
     update_js_file(src_images, options, js_output)
