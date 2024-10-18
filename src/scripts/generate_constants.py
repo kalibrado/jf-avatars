6bf2bd71c3d0f5163e31d0b770297dee42c78839
kalibrado <leonardo.fonseca85300@gmail.com>
2024-10-18 15:16:02 +0200

    fixbug: when retrieving the name of the parent folder which sets the options

--- src/scripts/generate_constants.py
+++ src/scripts/generate_constants.py
@@ -41,16 +41,13 @@ def generate_image_and_section_lists(images_dir, base_url):
     for root, _, files in walk(images_dir):
         for file in files:
             if file.endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg")):
-                relative_path = (
-                    path.join(root, file).replace(images_dir, "").lstrip(sep)
-                )
+                relative_path = (path.join(root, file).replace(images_dir, "").lstrip(sep))
+                section = relative_path.split("/")[0]
                 srcImages.add(base_url + relative_path)
-
-                section = file.split("-")[0]
                 options.add(section)
 
-    image_files_list = sorted(list(srcImages))
-    sections_list = sorted(list(options))
+    image_files_list = list(srcImages)
+    sections_list = list(options)
 
     return image_files_list, sections_list
 
